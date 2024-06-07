57d605604fce055255c7c083d18ec0cc831728fc
andriadvali <xuskiaxuski@gmail.com>
2024-06-07 19:01:33 +0400

    commit

---
+++ level 06/classwork/classwork.py
@@ -0,0 +1,27 @@
+num1 = 10
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+
+
+
+
+name = "David"
+surname = "Tezelashvili"
+print(name)
+print(surname)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 07/homework/homework2.py
@@ -0,0 +1,32 @@
+num1 = 11
+num2 = 2
+
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
