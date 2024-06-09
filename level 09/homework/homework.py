f6aaac81757daaa6db43d84b4b86d0de8e7e4065
andriadvali <xuskiaxuski@gmail.com>
2024-06-09 14:50:44 +0400

    commit

---
+++ level 09/homework/homework.py
@@ -0,0 +1,35 @@
+დავალება:1                                          
+#car-brend#არ უნდა ეწეროს - უნდა ეწეროს _
+#car brend#არ უნდა იყოს გამოტოვებული ენდა ეწეროს _  
+#car -> BWM#არ უნდა ეწეროს -> უნდა ეწეროს =
+#1NAME#არ უნდა წერა დაიწყო ასე
+#"ANDRIA'#არ უნდა "' უნდაეწეროს "" ან ''
+#(BWM)print#არ უნდა დაწერო ასე უნდა დწეარო ასე print(BWM)
+#PRint()#არ უნდა  დიდი ასოთი ნახევარი პატარა ასოთი 
+#print""#არ უნდა ეწეროს ასე print""  უნდა ეწეროს ასე print ("")
+#acaunt balance#არ უნდა ეწეროს acaunt balance უნდა ეწეროს ასე acaunt_balance
+#5computer#არ უნდა დაწერო რიცხვით 
+დავალება:2
+car_brrend = "mercedesi"
+acaunt_balance = "111"
+game_mesigs = "gameover"
+car_speed = "200"
+nem_gmail = "andria12@gmail.com"
+print(car_brrend)
+print(acaunt_balance)
+print(game_mesigs)
+print(car_speed)
+print(nem_gmail)
+დავალება:3
+#ჩვენ კომენტარს იმიტომ ვიყენებტთ რომ როცა რამე კოდი 
+#აღარ გვჭირდება დავაკომენტაროთ ჩვენ გიდევ დასათაურებაში ვიყენებტ კომენტარს
+
+
+
+
+
+
+
+
+
+
