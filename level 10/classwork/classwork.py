2d32db47e683b2255ebccc8be8b82a11c531e83f
andriadvali <xuskiaxuski@gmail.com>
2024-06-15 00:04:58 +0400

    commit

---
+++ level 10/classwork/classwork.py
@@ -0,0 +1,9 @@
+#about me
+my_name = "andria"
+my_surname = "dvali"
+my_age = 9
+#printing
+print(my_name)
+print(my_surname)
+print(my_age)
+
---
+++ level 11/homework/homewokk.py
@@ -0,0 +1,13 @@
+name = input("Enter you name: ")
+print(name)
+surname = input("Enter you surname: ")
+print(surname)
+age = input("Enter your age: ")
+print(age)
+#snake_case არის როცა სიტყვას ვწერთ ქვედატირედი მაგალიდათ ასე car_brend
+
+
+
+
+
+
