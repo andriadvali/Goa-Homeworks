007623d54fc7fef28503fcf2f51472bf60c27030
andriadvali <xuskiaxuski@gmail.com>
2024-06-30 15:29:53 +0400

    commit

---
+++ level 15/homework/homework.py
@@ -0,0 +1,19 @@
+name = input("enter you name: ")
+print(type(name))
+
+name = "andria"#str
+age = 9#int
+num = 9.5#float
+
+name = "andria" 
+num = 15
+num2 = 10.9
+print(type(name))
+print(type(num))
+print(type(num2))
+
+num = 20
+num2 = 4
+num3 = num/num2
+print(num3)
+print(type(num3))
