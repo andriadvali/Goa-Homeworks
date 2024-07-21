eca6295ecfd314731cfca1672b9722e5a2e1c393
andriadvali <xuskiaxuski@gmail.com>
2024-07-21 13:58:54 +0400

    commit

---
+++ level 20/homework/homework.py
@@ -0,0 +1,38 @@
+  #1:homework
+print(True or (3 < 2))
+print(True and (3 < 2))
+print(True or (3 < 2))
+print(True and (3 < 2))
+print(True or (3 < 2))
+print(True and (3 < 2))
+print(True or (3 < 2))
+print(True and (3 < 2))
+print(True or (3 < 2))
+print(True and (3 < 2))
+  #2:homework
+#print(12/6):
+#html-ში კომენტარის შესაქმნელად უნდა დავწეროთ:
+#პითონში კომენტარის შესაქმნელად უნდა დავწეროთ:
+#Boolean მონაცემიდ ტიპს აქვს სამი შესაძლო მნიშვნელობა:
+#რაეწოდება 0-იანებით და 1-იანებით სედმგარ კოდს:
+#html-ში საიტი ტექნიკური ინფორმაცია განთავსდება რომელ თეგში:
+#რა ეწოდება html-ში ერთი ელემენტის მეორე ელემენტში მოთავსებას?:
+#4 სფეისი იგივე რაც 1 ტაბი:
+#html-ში დალაგება სიის შესაქმნელად რომელი თეგი ვიყენებთ?:
+#კოდში კომენტარის დაწერა ცუდი პრაქტიკაა:
+#goa best?:
+  #3:homework
+#sequencing, iteration და selection არის სექვენსინგი არის თანმიმდევრობა, იტერათია არის განმეორიდებით 
+# დავალების შესრულება, სელექშენი არის პირობის მიხვედით გადაწყვეტილების მიღება
+  #4:homework
+name =input("enter you name:" )
+surname =input("enter you surname: ")
+age =int(input("enter you age: "))
+weigh =int(input("enter you weigh: "))
+mord = True
+
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(weigh))
+print(type(mord))
