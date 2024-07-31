619e4b0a2397e058e54d06f3001fc3616767a301
andriadvali <xuskiaxuski@gmail.com>
2024-07-31 13:19:10 +0400

    commit

---
+++ level 22/classwork/classwork.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+for num in range(10):
+    print(num + 2)
+
+for num in range(30):
+    print(num*2)
+
+    print(print)
---
+++ level 22/homemork/homework.py
@@ -0,0 +1,11 @@
+#1:homework
+for i in range(100):
+    print("GOA Best!")
+
+#2:homework
+for num in range(20):
+    print(num)
+
+#3:homework
+for num in range(15):
+    print(num * 2)
