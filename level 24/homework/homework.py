58f2b6495e1c019b7d80d574e5b270ebfb235552
andriadvali <xuskiaxuski@gmail.com>
2024-08-07 14:07:23 +0400

    commit

---
+++ level 24/homework/homework.py
@@ -0,0 +1,31 @@
+for i in range(10):
+    print("helo word!")
+    
+for i in range(30):
+    print("helo word!")
+
+for i in range(129):
+    print("helo word!")
+
+for i in range(1,10,2):
+    print(i)
+
+for i in range(12):
+    print("helo word!")
+
+for i in range(5):
+    print("helo word!")
+
+for i in range(10):
+    print("helo word!")
+
+for i in range(10):
+    print("helo word!")
+
+for i in range(10):
+    print("helo word!")
+
+
+
+
+    
