2613557beb6a8201b7fe12f7a8b74b4218901a74
andriadvali <xuskiaxuski@gmail.com>
2024-09-08 16:21:14 +0400

    commit

---
+++ level 34/classwork/classwork.py
@@ -0,0 +1,12 @@
+list1 =["ვაშლი", " ბანანი", "ქლიავი", "ატამი", "მსხალი"]
+print(list1[:2])
+print(list1[3:4])
+print(list1[:])
+
+
+list2 =["bwm", "audi", "toyoty", "jeep", "mercedes"] 
+print(list2[-1])
+
+name="andria"
+print(name.lower())
+print(name.upper())
---
+++ level 34/homework/homework.py
@@ -0,0 +1,4 @@
+name = input("enter you name: ")
+name2 = input("enter you mother name: ")
+print(name.upper())
+print(name2.lower())
