01623f2627e0ed4c5941615854f3dc66318a48a7
andriadvali <xuskiaxuski@gmail.com>
2024-09-12 16:41:44 +0400

    commit

---
+++ level 35.2/classwork/classwork.py
@@ -0,0 +1,7 @@
+fruit_in_basket=["apple", "banana", "watermelon", "dragonfrut", "orange"] 
+user_choise_of_fruit =input("ცაწერე შენი სყვარელი ხილი: ")
+
+if user_choise_of_fruit == "appel" or user_choise_of_fruit == "watermelon" or user_choise_of_fruit == "dragonfrute" or user_choise_of_fruit == "orange":
+    print("good chois")
+else:
+    print("there is not the fruit")
