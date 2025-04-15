90e468d6e8b643bdbae9768e2726a8e81870a67b
andriadvali <xuskiaxuski@gmail.com>
2025-04-15 19:35:38 +0400

    commit

--- level 51/classwork/classwork.py
+++
@@ -1,9 +0,0 @@
-dic = {
-    "name":"mercedes",
-    "color":"red",
-    "year":"2024",
-    "price":"400,000₾",
-
-}
-
-print(dic["name"])
---
+++ level 54/classwork/py.py
@@ -0,0 +1,18 @@
+user = {
+    "name" : "andria",
+    "surname" : "dvali",
+    "adrees" : "sackere,№6 agmasenebeli",
+    " hobbies" : ["football" , "programming"]
+}
+
+print(user)
+
+print(user["name"])
+print(user["surname"])
+print(user["adrees"])
+print(user[" hobbies"])
+
+print("name")
+print("urname")
+print("adrees")
+print(" hobbies")
